98bd564c6dc51f1e99696ed902589ba8a5c4faf9
Razi <zirachwidh@gmail.com>
2025-02-23 04:34:24 +0700

    refactor: AppTest.java (comment some lines)

--- src/app/src/test/java/src/AppTest.java
+++ src/app/src/test/java/src/AppTest.java
@@ -3,8 +3,8 @@
  */
 package src;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     // @Test void appHasAGreeting() {
