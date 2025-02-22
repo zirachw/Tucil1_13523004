d4431913cbacb82af0224e3a468c912e0d6ebe18
Razi <zirachwidh@gmail.com>
2025-02-23 04:35:34 +0700

    refactor: Gradle run input handler

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -44,6 +44,10 @@ application {
     mainClass = "src.Main"
 }
 
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
